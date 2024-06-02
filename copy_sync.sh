e6bc64b90a5db668b6b9d5354484ca885b1d3636
cvericel <47142250+cvericel@users.noreply.github.com>
2024-06-02 12:40:56 +0200

    Update copy_sync.sh

--- copy_sync.sh
+++ copy_sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # crackmapexec
-cp active-directory/enumeration/crackmapexec.md active-directory/post-exploitation/crackmapexec.md
+cp ./active-directory/enumeration/crackmapexec.md ./active-directory/post-exploitation/crackmapexec.md
